1c2b8061c277dd4d8fc1c897d5735d11c660a3bb
Nakul Jain <116788179+NakulJain-git@users.noreply.github.com>
2024-10-09 17:24:40 +0530

    second commit

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://flashify.pages.dev", "http://localhost:5173"],
+    origin: ["https://flashify-ai-frontend.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
