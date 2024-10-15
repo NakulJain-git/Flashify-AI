50bbe84507e32398b9021399ba93fe5019b58aa5
Nakul Jain <116788179+NakulJain-git@users.noreply.github.com>
2024-10-15 18:08:08 +0530

    changed loader

--- Frontend/src/components/Loader.jsx
+++ Frontend/src/components/Loader.jsx
@@ -1,15 +1,16 @@
-import { Hourglass } from "lucide-react"
-const Loader = () => {
-    return (
-        <div>
-            <Hourglass
-                visible="true"
-                size = "500"
-                color="#2563EB"
-                aria-label="infinity-spin-loading"
-            />
-        </div>
-    )
+import {Hourglass} from 'lucide-react'
+
+function Loader(){
+  return (
+    <div className="h-[50vh] flex justify-center items-center">
+      <Hourglass 
+        visible = "true"
+        size = "500"
+        color = "#2563EB"
+        aria-label='infinity-spin-loading'
+      />
+    </div>
+  )
 }
 
-export default Loader
+export default Loader;
